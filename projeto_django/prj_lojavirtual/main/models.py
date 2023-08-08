5fe02dd089db3b5f5f090560d8540f3c19b4f2bd
Matheus Menezes <matheus.menezes0806@gmail,com>
2023-08-08 16:55:01 -0300

    Praticando conceitos do Models em Django

---
+++ projeto_django/prj_lojavirtual/main/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True, db_index=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_ultima_atualizacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("nome", )
+        verbose_name = "categoria"
+        verbose_name_plural = "categorias"
+
+    def __str__(self):
+        return self.nome
+
+
+
+
+class Produto(models.Model):
+    #Criando um relacionamento um-para-muitos
+    categoria = models.ForeignKey(Categoria, related_name="produtos", null=True, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    estoque = models.DateTimeField(auto_now_add=True)
+    data_ultima_atualizacao = models.DateTimeField(auto_now=True)
+    imagem = models.ImageField(upload_to="imagens-produtos", blank=True)
+
+    class Meta:
+        ordering = ("nome", )
+        index_together = (("id", "slug"),)
+
+    # def save(self, *args, **kwargs):
+    #     print("O método save() foi acionado")
+    #     print(f"Parâmetros: *args: {args}, **kwargs: {kwargs}")
+    #     # Executa o método save da classe ancestral
+    #     super(Produto, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.nome
+    
+    
